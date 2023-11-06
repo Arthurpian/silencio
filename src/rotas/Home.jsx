5aa2fac34d80fb92890a887a9789578eca1f31fe
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-06 20:45:46 -0300

    carrosel

---
+++ src/rotas/Home.jsx
@@ -0,0 +1,37 @@
+import style from '../styles/home/home.module.css';
+import banner from '../imgs/banner.jpg';
+import data from "../json/livros.json"
+
+function Home() {
+  return (
+    <>
+      <div className={style.container}>
+        <div className={style.banner}>
+          <img src={banner} alt="banner" />
+        </div>
+        <div className={style.galeria}>
+          <div className={style.titulo}>
+            <h1>Mais vendidos</h1>
+          </div>
+          <div className={style.livros}>
+            <div className={style.carrosel}>
+                {data.map((item) => (
+                        <div className={style.id} key={item.id}>
+                        <div className={style.card}>
+                        <img src={item.capa} alt={item.nome} />
+                        </div>
+                        <div className={style.info}>
+                            <h1>{item.nome}</h1>
+                            <h2>{item.autor}</h2>
+                        </div>
+                        </div>
+                    ))}
+                </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
