991899f9cfc5c6dcb0211b080f1e919f0ae00fb9
arthur <arthurpianbessa@hotmail.com>
2023-11-06 07:44:36 -0300

    map

--- src/rotas/Home.jsx
+++ src/rotas/Home.jsx
@@ -1,6 +1,16 @@
 import style from '../styles/home/home.module.css'
 import banner from '../imgs/banner.jpg'
+import { useEffect, useState, useRef } from "react";
+
 function Home(){
+
+    fetch('http://localhost:5173/src/json/livros.json')
+    .then((Response) => Response.json())
+    .then(setData)
+    .catch((error) => {
+        console.error("Erro ao carregar os dados", error);
+    });
+
     return(
         <>
         <div className={style.container}>
@@ -12,7 +22,22 @@ function Home(){
                     <h1>Mais vendidos</h1>
                 </div>
                 <div className={style.livros}>
-                    
+                    <div className={style.carrosel}>
+                        {data.map((item) => {
+                            const { id, name, autor, capa } = item;
+                            return (
+                                <div className={style.id} key={id}>
+                                    <div className={style.card}>
+                                        <img src={capa} alt='capa' />
+                                    </div>
+                                    <div className={style.info}>
+                                        <h1>{name}</h1>
+                                        <h2>{autor}</h2>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
             </div>
         </div>
